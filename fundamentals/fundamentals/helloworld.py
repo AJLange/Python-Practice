106b6dac02b3a4b305a5926af4dee396e0ab59d6
Amanda Lange <litagemini@gmail.com>
2022-01-10 19:15:49 -0500

    Create helloworld.py

---
+++ fundamentals/fundamentals/helloworld.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+name = "Amanda"
+print("Hello ", name)
+print("Hello" + name)
+
+num = 7
+print("Hello", num, "!")
+print("Hello " + str(num))
+
+food_one = "steak"
+food_two = "pizza"
+
+print("I like to eat {0} and {1}".format(food_one, food_two))
+print(f"I like to eat {food_one} and {food_two}")
