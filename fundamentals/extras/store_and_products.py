4f74a0be38e7a61011341bdf202f6d6171af83b3
Amanda Lange <litagemini@gmail.com>
2022-01-16 18:10:16 -0500

    cards and store code - two barebones structures added

---
+++ fundamentals/extras/store_and_products.py
@@ -0,0 +1,14 @@
+class Store:
+
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+class Products:
+
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
+
+    
