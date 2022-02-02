3202e04fff85f52f5929d095f7fda2bca2000451
Amanda Lange <litagemini@gmail.com>
2022-02-01 19:14:34 -0500

    making new templates

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
@@ -8,12 +8,12 @@ def hello_world():
 def success():
     return "Dojo!"
 
-@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+@app.route('/hello/<name>') 
 def hello(name):
     print(name)
     return "Hi " + str(name)
 
-@app.route('/repeat/<count>/<word>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+@app.route('/repeat/<count>/<word>') 
 def repeat(count, word):   
 
         
@@ -26,10 +26,32 @@ def repeat(count, word):
     except ValueError:
         return "Please enter an integer for repeating."
 
+@app.route('/play') 
+def play():
+    
+    return render_template('index.html')  
+
+@app.route('/play/<number>') 
+def play_num(number):
+    try:
+        int(number)
+        return render_template('index2.html', num=int(number))  
+    except ValueError:
+        return "Only an integer value is acceptable here."
+    
+        
+@app.route('/play/<number>/<color>') 
+def play_col(number, color):
+    try:
+        int(number)
+        return render_template('index3.html', num=int(number), color=color)  
+
+    except ValueError:
+        return "Only an integer value is acceptable here."
+
 @app.errorhandler(404) 
 def not_found(e):
     return "Sorry! No response! Try again!"
-
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
